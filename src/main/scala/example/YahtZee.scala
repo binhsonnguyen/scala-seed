7f72c5161a21a7630f59e1633575bd42b73a7cfa
sonnb1812 <sonnb@vnext.vn>
2017-07-06 16:08:52 +0700

    straight

--- src/main/scala/example/YahtZee.scala
+++ src/main/scala/example/YahtZee.scala
@@ -9,6 +9,17 @@ object YahtZee {
     if (isFullHouse(ds)) ds.sum else 0
   }
 
+  def largeStraight(ds: Seq[Int]): Int = if (straightUntilLast(of = ds)(from = 2) == 6) 20 else 0
+
+  def smallStraight(ds: Seq[Int]): Int = if (straightUntilLast(of = ds)(from = 1) == 5) 15 else 0
+
+  @annotation.tailrec
+  private def straightUntilLast(of: Seq[Int])(from: Int): Int = of match {
+    case last +: Seq() => if (from == last) from else 0
+    case head +: tail => if (from == head) straightUntilLast(tail)(from + 1) else 0
+    case _ => 0
+  }
+
   private def isFullHouse(ds: Seq[Int]): Boolean = {
     val g = groupsAndCount(ds)
     g.size == 2 && (g.head._2 == 2 || g.head._2 == 3)
--- src/test/scala/example/YahtZeeSpec.scala
+++ src/test/scala/example/YahtZeeSpec.scala
@@ -15,11 +15,24 @@ class YahtZeeSpec extends FlatSpec with Matchers {
     YahtZee.yahtzee(List(6, 6, 6, 6, 1)) should be (0)
   }
 
-  "fullhouse" should "give sumation if fullhouse else zero" in {
+  it should "give sumation if successful Full House else zero" in {
     YahtZee.fullhouse(List(1, 1, 2, 2, 2)) should be (8)
-    YahtZee.fullhouse(List(1, 2, 1, 2, 2)) should be (8)
+    YahtZee.fullhouse(List(3, 2, 3, 2, 2)) should be (12)
     YahtZee.fullhouse(List(1, 1, 2, 2, 3)) should be (0)
     YahtZee.fullhouse(List(1, 2, 2, 2, 3)) should be (0)
     YahtZee.fullhouse(List(1, 2, 2, 3, 3)) should be (0)
+    YahtZee.fullhouse(List(6, 6, 6, 6, 6)) should be (0)
   }
+
+  it should "give 20 if successful Large Straight else zero" in {
+    YahtZee.largeStraight(List(2, 3, 4, 5, 6)) should be (20)
+    YahtZee.largeStraight(List(2, 3, 4, 5, 5)) should be (0)
+  }
+
+  it should "give 15 if successful Small Straight else zero" in {
+    YahtZee.smallStraight(List(1, 2, 3, 4, 5)) should be (15)
+    YahtZee.smallStraight(List(2, 3, 4, 5, 6)) should be (0)
+    YahtZee.smallStraight(List(1, 2, 3, 4, 4)) should be (0)
+  }
+
 }
