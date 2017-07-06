15095809c7ab0ae3e4172f9cae6a55f9196e1504
sonnb1812 <sonnb@vnext.vn>
2017-07-06 14:20:44 +0700

    refactor fullhouse

--- src/main/scala/example/YahtZee.scala
+++ src/main/scala/example/YahtZee.scala
@@ -6,8 +6,14 @@ object YahtZee {
   def yahtzee(ds: Seq[Int]): Int = if (ds.tail.forall(d => d == ds.head)) 50 else 0
 
   def fullhouse(ds: Seq[Int]): Int = {
-    val groupsCount = ds.groupBy(d => d).map(g => (g._1, g._2.size))
-    if (groupsCount.count(g => g._2 == 2) == 1 && groupsCount.count(g => g._2 == 3) == 1) ds.sum else 0
+    if (isFullHouse(ds)) ds.sum else 0
   }
+
+  private def isFullHouse(ds: Seq[Int]): Boolean = {
+    val g = groupsAndCount(ds)
+    g.size == 2 && (g.head._2 == 2 || g.head._2 == 3)
+  }
+
+  private def groupsAndCount(ds: Seq[Int]): Map[Int, Int] = ds.groupBy(d => d).map(g => (g._1, g._2.size))
 
 }
